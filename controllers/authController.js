48b4911c13eae58a65815ed0f835d27dea92b16c
G Key <g.key0325@gmail.com>
2025-01-20 07:10:24 -0500

    modify

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,7 +8,6 @@ require("dotenv").config();
 const gmail_user = process.env.GMAIL_USER;
 const gmail_password = process.env.GMAIL_PASS;
 const frontend_url = process.env.FRONTEND_URL;
-console.log(gmail_user)
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -22,41 +21,35 @@ const registerUser = async (req, res) => {
   
   const { name, email, password } = req.body;
 
-  try {
+  const user = await User.findOne({ email });
+  
+  if (user && user.isVerified) return res.status(400).json({ error: 'Email already exist.' });
 
-    const user = await User.findOne({ email });
-    
-    if (user) return res.status(400).json({ error: 'Email already exist.' });
-
+  if (!user) {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const verificationToken = jwt.sign({ email }, JWT_SECRET, { expiresIn: '1d' });
     const newUser = new User({ name, email, password: hashedPassword, verificationToken });
-
     newUser.save();
+  }    
+  
+  const verificationToken = jwt.sign({ email }, JWT_SECRET, { expiresIn: '1d' });
+  const verificationUrl = `${frontend_url}verify-email?token=${verificationToken}`;
 
-    const verificationUrl = `${frontend_url}verify-email?token=${verificationToken}`;
-
-    const mailOptions = {
-      from: gmail_user,
-      to: email,
-      subject: 'Email Verification',
-      html: `<p>Click <a href="${verificationUrl}">here</a> to verify your email address.</p>`,
-    };
-
-    console.log(mailOptions)
-
-    try {
-      await transporter.sendMail(mailOptions);
-      res.status(200).json({ message: 'Registration successful. Please check your email to verify your account.' });
-    } catch (error) {
-      res.status(500).json({ message: 'Failed to send verification email.' });
-    }
-
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: 'Error registering user' });
+  const mailOptions = {
+    from: gmail_user,
+    to: email,
+    subject: 'Email Verification',
+    html: `<p>Click <a href="${verificationUrl}">here</a> to verify your email address.</p>`,
+  };
+  console.log(mailOptions);
+  
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: 'Registration successful. Please check your email to verify your account.' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Failed to send verification email.' });
   }
-};
+}
 
 const verifyEmail = async (req, res) => {
   const { token } = req.query;
@@ -85,34 +78,31 @@ const verifyEmail = async (req, res) => {
 const loginUser = async (req, res) => {
 
   const { email, password } = req.body;
-
-  try {
-
+  console.log(email, password);
+  
     const user = await User.findOne({ email });
 
-    if (!user) {
-      return res.status(400).json({ error: 'Invalid email!' });
-    }
+    if (!user) return res.status(400).json({ error: 'Invalid email!' });
 
+    if (!user.isVerified && !user.accessToken) return res.status(400).json({ error: 'Email is not verified!' })
+      
+    if (user.accessToken) return res.status(400).json({ error: 'Please login with Google' });
+      
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ error: 'Invalid password!' });
-    }
-
-    if (!user.isVerified) return res.status(400).json({ error: 'Email is not verified!' })
+    
+    if (!isMatch) return res.status(400).json({ error: 'Invalid password!' });
 
     const token = jwt.sign({ name: user.name, email: user.email }, JWT_SECRET, { expiresIn: '1d' });
 
     res.status(200).json({ token });
 
-  } catch (err) {
-    res.status(500).json({ error: 'Error logging in' });
-  }
 };
 
 const googleLogin = async (req, res) => {
 
   const { email, name, accessToken } = req.body;
+
+  console.log(email, name, accessToken)
 
   try {
 
@@ -128,7 +118,7 @@ const googleLogin = async (req, res) => {
 
   } catch (err) {
     console.log(err)
-    res.status(500).json({ error: 'Error loggin with Google' });
+    res.status(500).json({ error:err });
   }
 }
 
